98c0a91021ac7ba86f50cf391d2d9aac82d46a01
Fionn Fitzmaurice <fionn@github.com>
2022-01-23 18:29:26 +0800

    initial commit

---
+++ french_fries/french_fries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""How many ways are there to eat a dish of 99999 french fries?"""
+
+
+def ff(n: int) -> int:
+    """Number of ways of eating n fries, either one or two at a time"""
+    a, b = 1, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+
+def main() -> None:
+    """Entry point"""
+    print(ff(99999))
+
+if __name__ == "__main__":
+    main()

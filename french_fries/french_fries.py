ccf19b503e9b1fa26f20c07d5a403fabc9ae8197
Fionn Fitzmaurice <fionn@github.com>
2024-01-06 22:40:25 +0800

    Disable integer string conversion length limit

--- french_fries/french_fries.py
+++ french_fries/french_fries.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """How many ways are there to eat a dish of 99999 french fries?"""
 
+import sys
+
+sys.set_int_max_str_digits(0)
+
 
 def ff(n: int) -> int:
     """Number of ways of eating n fries, either one or two at a time"""
